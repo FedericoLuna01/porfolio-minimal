81174a0558c56212e7ab87c5b74e1cf52e69b34c
Federico Luna <fluna@frro.utn.edu.ar>
2024-04-10 20:22:38 -0300

    update

--- components/projects/project-card.tsx
+++ components/projects/project-card.tsx
@@ -1,99 +1,68 @@
-import Link from "next/link"
-import { ExternalLink, Github } from "lucide-react"
-import Image from "next/image"
+import Link from "next/link";
+import { ExternalLink, Github } from "lucide-react";
+import Image from "next/image";
 
-import {
-  Collapsible,
-  CollapsibleContent,
-} from "@/components/ui/collapsible"
-import { Button } from "@/components/ui/button"
-import ViewMore from "./viewmore-button"
+import { Collapsible, CollapsibleContent } from "@/components/ui/collapsible";
+import { Button } from "@/components/ui/button";
+import ViewMore from "./viewmore-button";
 
 interface ProjectCardProps {
   project: {
-    title: string
-    description: string
-    repo: string
-    demo: string
-    images: string[]
-    technogies: string[]
-  }
+    title: string;
+    description: string;
+    repo: string;
+    demo: string;
+    images: string[];
+    technologies: string[];
+  };
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
+const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
-    <Collapsible
-      className="border border-gray-400 transition-all rounded-md p-2 mb-4"
-    >
-      <h3 className="text-lg">
-        {project.title}
-      </h3>
-      <p
-        className="text-zinc-600 dark:text-zinc-400 mr-0 md:mr-10"
-      >
+    <Collapsible className="border border-gray-400 transition-all rounded-md p-2 mb-4">
+      <h3 className="text-lg">{project.title}</h3>
+      <p className="text-zinc-600 dark:text-zinc-400 mr-0 md:mr-10">
         {project.description}
       </p>
       <CollapsibleContent>
         <div>
           <h4 className="mb-1 mt-2">Tecnologías</h4>
           <ul className="flex flex-wrap space-x-2">
-            {
-              project.technogies.map((tech, index) => (
-                <li
-                  key={index}
-                  className="border border-zinc-600 dark:border-zinc-400 text-zinc-600 dark:text-zinc-400 rounded-md px-2 py-1 text-sm"
-                >
-                  {tech}
-                </li>
-              ))
-            }
+            {project.technologies.map((tech, index) => (
+              <li
+                key={index}
+                className="border border-zinc-600 dark:border-zinc-400 text-zinc-600 dark:text-zinc-400 rounded-md px-2 py-1 text-sm"
+              >
+                {tech}
+              </li>
+            ))}
           </ul>
         </div>
         <div className="pb-1">
           <h4 className="mb-1 mt-2">Demo</h4>
-          <div
-            className="flex space-y-2 justify-between flex-col"
-          >
-            {
-              project.images.map((image, index) => (
-                <Image
-                  key={index}
-                  src={image}
-                  width={450}
-                  height={450}
-                  alt='Demo'
-                  className="rounded-md object-cover"
-                />
-              ))
-            }
+          <div className="flex space-y-2 justify-between flex-col">
+            {project.images.map((image, index) => (
+              <Image
+                key={index}
+                src={image}
+                width={450}
+                height={450}
+                alt="Demo"
+                className="rounded-md object-cover"
+              />
+            ))}
           </div>
         </div>
       </CollapsibleContent>
-      <div
-        className="flex justify-between items-center pt-2"
-      >
+      <div className="flex justify-between items-center pt-2">
         <div className="space-x-2">
-          <Button
-            size='icon'
-            variant='secondary'
-            asChild
-          >
-            <Link
-              href={project.demo}
-              target="_blank"
-            >
+          <Button size="icon" variant="secondary" asChild>
+            <Link href={project.demo} target="_blank">
               <ExternalLink size={24} />
             </Link>
           </Button>
-          <Button
-            size='icon'
-            variant='secondary'
-            asChild
-          >
-            <Link
-              href={project.repo}
-              target="_blank"
-            >
+          <Button size="icon" variant="secondary" asChild>
+            <Link href={project.repo} target="_blank">
               <Github size={24} />
             </Link>
           </Button>
@@ -101,7 +70,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
         <ViewMore />
       </div>
     </Collapsible>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- components/tech-stack/tech-stack.tsx
+++ components/tech-stack/tech-stack.tsx
@@ -1,36 +1,18 @@
-import { StackItems } from "@/data/data"
+import { StackItems } from "@/data/data";
 
 const TechStack = () => {
   return (
-    <section
-      className="pt-10"
-    >
-      <h2
-        className="text-xl mb-2"
-      >
-        Este es mi tech stack favorito
-      </h2>
-      <div
-        className="flex gap-5 flex-wrap"
-      >
-        {
-          StackItems.map(({ id, name, icon }) => (
-            <div
-              key={id}
-              className="flex gap-2"
-            >
-              <div
-                className="w-auto h-auto"
-              >
-                {icon}
-              </div>
-              <p className="text-zinc-600 dark:text-zinc-400">{name}</p>
-            </div>
-          ))
-        }
+    <section className="mt-10">
+      <h2 className="text-xl mb-2">Este es mi tech stack favorito</h2>
+      <div className="flex gap-6 flex-wrap">
+        {StackItems.map(({ id, name }) => (
+          <p key={id} className="text-muted-foreground">
+            {name}
+          </p>
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default TechStack
+export default TechStack;
